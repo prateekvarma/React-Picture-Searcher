69cfb75d2ff9da55a95d575e565f8fb530af8e4a
Prateek Varma <prtk.varma@gmail.com>
2022-05-29 11:37:58 -0600

    wrote basic logic of handling text input on a form with an external function - with 'uncontrolled Element'

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,19 @@
 import React from "react";
 
+//Used Class based component, because we need states, & Class based components are easier with States.
 class SearchBar extends React.Component {
+  onInputChange(event) {
+    console.log(event.target.value);
+  }
+
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="field">
-              <label>Image Search</label>
-            <input type="text" />
+            <label>Image Search</label>
+            {/* Below, we do not add () to onInputChange function, because we do not need to call the function by ourselves. By not using (), we are simply refering to it's instance which will be called by the input field as and when needed. */}
+            <input type="text" onChange={this.onInputChange} />
           </div>
         </form>
       </div>
