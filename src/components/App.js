1d9c6bb6fb24386d5588972de5d9ec41f83d58de
Prateek Varma <prtk.varma@gmail.com>
2022-05-31 14:41:26 -0600

    installed axios, and made a GET request to Unsplash API (key not included)

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    //Below, 'term' is a variable passed from a child to parent component
-    console.log(term);
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID ACCESS_KEY_HERE'
+      }
+    })
   }
 
   render() {
