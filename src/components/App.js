11c72e0f2459677c9be6d0d36045dd5a13f45f35
Prateek Varma <prtk.varma@gmail.com>
2022-06-01 15:52:09 -0600

    Used state to store images. Also fixed userState's context binding issue by converting into arrow function

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,9 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  async onSearchSubmit(term) {
+  state = { images: [] };
+
+  onSearchSubmit = async (term) => {
     const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: term },
       headers: {
@@ -11,12 +13,14 @@ class App extends React.Component {
       },
     });
     console.log(response.data.results);
-  }
+    this.setState({ images: response.data.results });
+  };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onSubmitData={this.onSearchSubmit} />
+        Found {this.state.images.length} images!
       </div>
     );
   }
