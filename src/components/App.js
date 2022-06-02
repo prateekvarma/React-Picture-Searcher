74cc000bff3bb7b39c9f5c58de69debf79acbc0a
Prateek Varma <prtk.varma@gmail.com>
2022-06-02 16:27:35 -0600

    created ImageList component, and used it to display search results from Unsplash API

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import SearchBar from "./SearchBar";
+import ImageList from "./ImageList";
 
 class App extends React.Component {
   state = { images: [] };
@@ -12,7 +13,7 @@ class App extends React.Component {
         Authorization: "Client-ID ACCESS_KEY",
       },
     });
-    console.log(response.data.results);
+    //console.log(response.data.results);
     this.setState({ images: response.data.results });
   };
 
@@ -21,6 +22,7 @@ class App extends React.Component {
       <div className="ui container">
         <SearchBar onSubmitData={this.onSearchSubmit} />
         Found {this.state.images.length} images!
+        <ImageList images={this.state.images} />
       </div>
     );
   }
