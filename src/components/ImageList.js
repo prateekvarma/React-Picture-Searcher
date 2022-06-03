de540bbee4acb3105880ff3a5e0128d40447620c
Prateek Varma <prtk.varma@gmail.com>
2022-06-03 16:10:58 -0600

    created a new ImageCard component for each individual image - this is to adjust some css properties(for the next commit)

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,10 +1,11 @@
 import React from "react";
+import ImageCard from "./ImageCard";
 import "./ImageList.css";
 
 const ImageList = (props) => {
   const images = props.images.map((image) => {
     return (
-      <img src={image.urls.regular} alt={image.description} key={image.id} />
+      <ImageCard image={image} key={image.id} />
     );
   });
   return <div className="image-list">{images}</div>;
