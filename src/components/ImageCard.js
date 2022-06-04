f78638783e27a86b21d3d204776405201e1eca22
Prateek Varma <prtk.varma@gmail.com>
2022-06-04 16:36:40 -0600

    Wiring a 'ref' to handle the image's DOM's height attribute, and save it in a variable imageRef

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,10 +1,18 @@
 import React from "react";
 
 class ImageCard extends React.Component {
+  //Below, in this caomponent, the only use of creating the constructor is to create a "Ref" - in order to get the image's DOM information from the external API
+  constructor(props) {
+    super(props);
+    //Below, "imageRef" is an instance variable, that will contain the value of the Ref
+    this.imageRef = React.createRef();
+  }
+
   render() {
     return (
       <div>
         <img
+          ref={this.imageRef}
           alt={this.props.image.description}
           src={this.props.image.urls.regular}
         />
