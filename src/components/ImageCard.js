28240245f911d5931eeb16a7b8d1c27d23faceb3
Prateek Varma <prtk.varma@gmail.com>
2022-06-05 12:03:29 -0600

    used state variable, and assigned value from ref to it, which is finally used to set a CSS property

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -4,23 +4,26 @@ class ImageCard extends React.Component {
   //Below, in this caomponent, the only use of creating the constructor is to create a "Ref" - in order to get the image's DOM information from the external API
   constructor(props) {
     super(props);
+    this.state = { spans: 0 };
     //Below, "imageRef" is an instance variable, that will contain the value of the Ref
     this.imageRef = React.createRef();
   }
 
   componentDidMount() {
     // Below, we cannot just do 'this.imageRef.current.clientHeight' because the clientHeight property does not load in time. So we use an event Listener.
-    this.imageRef.current.addEventListener('load', this.setSpans);
+    this.imageRef.current.addEventListener("load", this.setSpans);
   }
 
   //Below setSpans is a callback function, that is defined arrow, as it takes care of the bind problem in React. This callback function will be able to extract each image's dimentions
   setSpans = () => {
-    console.log(this.imageRef.current.clientHeight);
-  }
+    const height = this.imageRef.current.clientHeight;
+    const spans = Math.ceil(height / 150);
+    this.setState({ spans: spans });
+  };
 
   render() {
     return (
-      <div>
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
         <img
           ref={this.imageRef}
           alt={this.props.image.description}
